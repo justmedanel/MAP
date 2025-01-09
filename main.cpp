c75d34d046db9674880abbe6ac8425ba33e2d039
justmedanel <daniel.ursu3@student.upt.ro>
2025-01-09 22:29:33 +0200

    initializare si afisare mat

--- main.cpp
+++ main.cpp
@@ -11,7 +11,27 @@ int main(){
     cin >> m;
     cout << "Introdu nr de coloane: ";
     cin >> n;
+
+    if(m!=n){
+        cout << "Matricea nu este patrata" << endl;
+        return 0;
+    }
+
     int matrice[m][n];
-    
+
+    srand(time(0));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            matrice[i][j] = rand()%100;
+        }
+    }
+    cout << "Matricea: " << endl;
+    for(int i = 0;i<m;i++){
+        for(int j = 0; j < n; j++){
+            cout << matrice[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
