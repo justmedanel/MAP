c7e1bdd4b6351be03497897f7152602bf69e49db
justmedanel <daniel.ursu3@student.upt.ro>
2025-01-09 23:02:21 +0200

    afisare max/min sub diagonale

--- main.cpp
+++ main.cpp
@@ -41,6 +41,35 @@ int main(){
     }
     cout << "Suma elementelor pe diagonala principala: " << sumaDiagPrincipala << endl;
     cout << "Suma elementelor pe diagonala secundara: " << sumaDiagSecundara << endl;
+    
+    int minprincipala = 100;
+    int maxprincipala = -1;
+    int minsecundara = 100;
+    int maxsecundara = -1;
 
+    for(int i = 1; i < m; i++){
+        for(int j = 0; j < i; j ++){
+            if(matrice[i][j] < minprincipala){
+                minprincipala = matrice[i][j];
+            }
+            if(matrice[i][j] > maxprincipala){
+                maxprincipala = matrice[i][j];
+            }
+        }
+        for(int j = m-i; j < m; j++){
+            if(matrice[i][j] < minsecundara){
+                minsecundara = matrice[i][j];
+            }
+            if(matrice[i][j] > maxsecundara){
+                maxsecundara = matrice[i][j];
+            }
+
+        }
+    }
+
+    cout << "Minimul sub diagonala principala: " << minprincipala << endl;
+    cout << "Maximul sub diagonala principala: " << maxprincipala << endl;
+    cout << "Minimul sub diagonala secundara: " << minsecundara << endl;
+    cout << "Maximul sub diagonala secundara: " << maxsecundara << endl;
     return 0;
 }
